bcb871a1c002a1b6a7cb4fe7603db1f3014f8a92
Eric Vanular <eric.vanular@gmail.com>
2021-11-15 23:52:46 -0500

    Ship it already 3

--- pages/hosts/[host]/index.js
+++ pages/hosts/[host]/index.js
@@ -15,55 +15,54 @@ const MAX_DISPLAY = 5
 import { useKeycloak } from '@react-keycloak/ssr'
 
 export async function getStaticPaths() {
-	return {
-		paths: [],
-		fallback: 'blocking',
-	}
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
 }
 
 export async function getStaticProps(context) {
-	const host = context.params.host
-	//const postsData = await API(`https://app.jetpeak.co/app/api/creator/posts/eric`)
-	const profileData = await API(`https://app.jetpeak.co/app/api/creator/${host.split('.')[0]}`)
+  const host = context.params.host
+  //const postsData = await API(`https://app.jetpeak.co/app/api/creator/posts/eric`)
+  const profileData = await API(`https://app.jetpeak.co/app/api/creator/${host.split('.')[0]}`)
 
-	if (!profileData.result) {
-		return {
-			notFound: true,
-		}
-	}
+  if (!profileData.result) {
+    return {
+      notFound: true,
+    }
+  }
 
-	const { id, username, description, avatar_url, banner_url } = profileData.creator
-	//const { posts } = postsData
+  const { id, username, description, avatar_url, banner_url } = profileData.creator
+  //const { posts } = postsData
 
-	return {
-		props: {
-			//posts,
-			host,
-			id,
-			username,
-			description,
-            avatar_url,
-            banner_url
-		},
-		// revalidate: 10,
-	}
+  return {
+    props: {
+      //posts,
+      host,
+      id,
+      username,
+      description,
+      avatar_url,
+      banner_url,
+    },
+    // revalidate: 10,
+  }
 }
 
 export default function Home({ host, id, username, description, avatar_url, banner_url }) {
+  const [redirect, setRedirect] = useState(false)
+  const [creator, setCreator] = useState({})
+  const [posts, setPosts] = useState([])
+  const [paymentMethods, setPaymentMethods] = useState([])
+  const [showTipModal, setShowTipModal] = useState(false)
+  const [showFollowModal, setShowFollowModal] = useState(false)
+  const [showUnfollowModal, setShowUnfollowModal] = useState(false)
 
-    const [redirect, setRedirect] = useState(false)
-    const [creator, setCreator] = useState({})
-    //const [posts, setPosts] = useState([])
-    const [paymentMethods, setPaymentMethods] = useState([])
-    const [showTipModal, setShowTipModal] = useState(false)
-    const [showFollowModal, setShowFollowModal] = useState(false)
-    const [showUnfollowModal, setShowUnfollowModal] = useState(false)
+  const { keycloak } = useKeycloak()
 
-	const { keycloak } = useKeycloak()
+  const parsedToken = keycloak?.tokenParsed
 
-	const parsedToken = keycloak?.tokenParsed
-
-	/*
+  /*
   const { data, error, isValidating } = useSWR(
     `http://localhost:5000/app/api/creator/posts/${host.split('.')[0]}`,
     (url) =>
@@ -80,80 +79,110 @@ export default function Home({ host, id, username, description, avatar_url, bann
   )
   */
 
-	const fetchWithToken = async (url, token) => {
-		const res = await fetch(url, {
-			headers: {
-				Authorization: `Bearer ${token}`,
-			},
-		})
-		return res.json()
-	}
+  const fetchWithToken = async (url, token) => {
+    const res = await fetch(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    return res.json()
+  }
 
-    const getPaymentMethods = () => {
-        const options = {
-            method: 'get',
-            url: '/api/subscription/payment_method'
-        };
-        API(options).then(data => setPaymentMethods(data.payment_methods))
+  const getCreator = () => {
+    const options = {
+      method: 'get',
+      url: `http://localhost:5000/app/api/creator/${host}`,
     }
+    API(options)
+      .then((data) => setCreator(data.creator))
+      .catch(() => setRedirect(true))
+  }
 
-    const { data: paymentMethodsData, error: paymentMethodsError } = useSWR(
-		keycloak?.token ? [`http://localhost:5000/api/subscription/payment_method`, keycloak?.token] : null,
-		fetchWithToken
-	)
+  const getPosts = (page) => {
+    const options = {
+      method: 'get',
+      url: username
+        ? `/app/api/creator/posts/${username}?page=${page}`
+        : `/app/api/creator/posts?page=${page}`,
+    }
+    API(options)
+      .then((data) => {
+        setPosts(data.posts)
+      })
+      .catch(() => setRedirect(true))
+  }
 
-	const { data: postsData, error: postsError } = useSWR(
-		[`http://localhost:5000/app/api/creator/posts/${host.split('.')[0]}`, keycloak?.token],
-		fetchWithToken
-	)
-    
-    {/*
+  const getPaymentMethods = () => {
+    const options = {
+      method: 'get',
+      url: '/api/subscription/payment_method',
+    }
+    API(options).then((data) => setPaymentMethods(data.payment_methods))
+  }
+
+  const { data: paymentMethodsData, error: paymentMethodsError } = useSWR(
+    keycloak?.token
+      ? [`http://localhost:5000/api/subscription/payment_method`, keycloak?.token]
+      : null,
+    fetchWithToken
+  )
+
+  const { data: postsData, error: postsError } = useSWR(
+    [`http://localhost:5000/app/api/creator/posts/${host.split('.')[0]}`, keycloak?.token],
+    fetchWithToken
+  )
+
+  {
+    /*
     useEffect(() => {
 		if (props.username) {
 			getPlans()
 		}
 	}, [props.username])
-    */}
+    */
+  }
 
-	return (
-		<>
-			<PageSEO title={host} description={siteMetadata.description} />
-            <FollowModal
-                open={showFollowModal}
-                setOpen={setShowFollowModal}
-                username={username}
-                getCreator={getCreator}
-                getPosts={getPosts}
-                banner_url={banner_url}
-                avatar_url={avatar_url}
-                paymentMethods={paymentMethodsData?.payment_methods}
-                getPaymentMethods={getPaymentMethods}
-                
-            />
-            <TipModal
-                open={showTipModal}
-                setOpen={setShowTipModal}
-                username={username}
-                paymentMethods={paymentMethodsData?.payment_methods}
-                getPaymentMethods={getPaymentMethods}
-                
-            />
-			<div className="divide-y divide-gray-200 dark:divide-gray-700">
-				<div className="pb-8 space-y-2 md:space-y-5">
-					<h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-						{username}
-					</h1>
-					<p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-						{description}
-					</p>
-                    <div className="flex flex-row items-start justify-start">
-                        <button onClick={() => setShowFollowModal(true)} className="bg-transparent hover:bg-indigo-600 text-indigo-700 font-semibold hover:text-white py-2 px-4 mx-2 border border-indigo-800 hover:border-transparent rounded">
-                            Follow
-                        </button>
-                        <button onClick={() => setShowTipModal(true)} className="bg-transparent hover:bg-green-600 text-indigo-700 font-semibold hover:text-white py-2 px-4 mx-2 border border-green-800 hover:border-transparent rounded">
-                            Tip
-                        </button>
-                        {/*
+  return (
+    <>
+      <PageSEO title={host} description={siteMetadata.description} />
+      <FollowModal
+        open={showFollowModal}
+        setOpen={setShowFollowModal}
+        username={username}
+        getCreator={getCreator}
+        getPosts={getPosts}
+        banner_url={banner_url}
+        avatar_url={avatar_url}
+        paymentMethods={paymentMethodsData?.payment_methods}
+        getPaymentMethods={getPaymentMethods}
+      />
+      <TipModal
+        open={showTipModal}
+        setOpen={setShowTipModal}
+        username={username}
+        paymentMethods={paymentMethodsData?.payment_methods}
+        getPaymentMethods={getPaymentMethods}
+      />
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="pb-8 space-y-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            {username}
+          </h1>
+          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">{description}</p>
+          <div className="flex flex-row items-start justify-start">
+            <button
+              onClick={() => setShowFollowModal(true)}
+              className="bg-transparent hover:bg-indigo-600 text-indigo-700 font-semibold hover:text-white py-2 px-4 mx-2 border border-indigo-800 hover:border-transparent rounded"
+            >
+              Follow
+            </button>
+            <button
+              onClick={() => setShowTipModal(true)}
+              className="bg-transparent hover:bg-green-600 text-indigo-700 font-semibold hover:text-white py-2 px-4 mx-2 border border-green-800 hover:border-transparent rounded"
+            >
+              Tip
+            </button>
+            {/*
                         <Link
                             href={`/chat`}
                             className="bg-transparent hover:bg-pink-600 text-indigo-700 font-semibold hover:text-white py-2 px-4 mx-2 border border-pink-800 hover:border-transparent rounded"
@@ -161,21 +190,21 @@ export default function Home({ host, id, username, description, avatar_url, bann
                             Chat
                         </Link>
                         */}
-                    </div>
-				</div>
-				<PostGrid posts={postsData?.posts} creatorId={id} setShow={setShowFollowModal} />
-			</div>
-			{postsData?.posts?.length > MAX_DISPLAY && (
-				<div className="flex justify-end text-base font-medium leading-6">
-					<Link
-						href="/blog"
-						className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
-						aria-label="all posts"
-					>
-						All Posts &rarr;
-					</Link>
-				</div>
-			)}
-		</>
-	)
+          </div>
+        </div>
+        <PostGrid posts={postsData?.posts} creatorId={id} setShow={setShowFollowModal} />
+      </div>
+      {postsData?.posts?.length > MAX_DISPLAY && (
+        <div className="flex justify-end text-base font-medium leading-6">
+          <Link
+            href="/blog"
+            className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
+            aria-label="all posts"
+          >
+            All Posts &rarr;
+          </Link>
+        </div>
+      )}
+    </>
+  )
 }
--- pages/hosts/[host]/posts/[...slug].js
+++ pages/hosts/[host]/posts/[...slug].js
@@ -21,57 +21,57 @@ export async function getStaticPaths() {
 */
 
 export async function getStaticPaths() {
-	return {
-		paths: [],
-		fallback: 'blocking',
-	}
+  return {
+    paths: [],
+    fallback: 'blocking',
+  }
 }
 
 export async function getStaticProps({ params }) {
-	const allPosts = await getAllFilesFrontMatter('blog')
-	const postIndex = allPosts.findIndex((post) => formatSlug(post.slug) === params.slug.join('/'))
-	const prev = allPosts[postIndex + 1] || null
-	const next = allPosts[postIndex - 1] || null
-	const post = await getFileBySlug('blog', params.slug.join('/'))
-	const authorList = post.frontMatter.authors || ['default']
-	const authorPromise = authorList.map(async (author) => {
-		const authorResults = await getFileBySlug('authors', [author])
-		return authorResults.frontMatter
-	})
-	const authorDetails = await Promise.all(authorPromise)
+  const allPosts = await getAllFilesFrontMatter('blog')
+  const postIndex = allPosts.findIndex((post) => formatSlug(post.slug) === params.slug.join('/'))
+  const prev = allPosts[postIndex + 1] || null
+  const next = allPosts[postIndex - 1] || null
+  const post = await getFileBySlug('blog', params.slug.join('/'))
+  const authorList = post.frontMatter.authors || ['default']
+  const authorPromise = authorList.map(async (author) => {
+    const authorResults = await getFileBySlug('authors', [author])
+    return authorResults.frontMatter
+  })
+  const authorDetails = await Promise.all(authorPromise)
 
-	// rss
-	const rss = generateRss(allPosts)
-	fs.writeFileSync('./public/feed.xml', rss)
+  // rss
+  const rss = generateRss(allPosts)
+  fs.writeFileSync('./public/feed.xml', rss)
 
-	return { props: { post, authorDetails, prev, next } }
+  return { props: { post, authorDetails, prev, next } }
 }
 
 export default function Blog({ post, authorDetails, prev, next }) {
-	const { mdxSource, toc, frontMatter } = post
+  const { mdxSource, toc, frontMatter } = post
 
-	return (
-		<>
-			{/*frontMatter.draft !== true*/ 1===2 ? (
-				<MDXLayoutRenderer
-					layout={frontMatter.layout || DEFAULT_LAYOUT}
-					toc={toc}
-					mdxSource={mdxSource}
-					frontMatter={frontMatter}
-					authorDetails={authorDetails}
-					prev={prev}
-					next={next}
-				/>
-			) : (
-				<div className="mt-24 text-center">
-					<PageTitle>
-						Under Construction{' '}
-						<span role="img" aria-label="roadwork sign">
-							🚧
-						</span>
-					</PageTitle>
-				</div>
-			)}
-		</>
-	)
+  return (
+    <>
+      {frontMatter.draft !== true ? (
+        <MDXLayoutRenderer
+          layout={frontMatter.layout || DEFAULT_LAYOUT}
+          toc={toc}
+          mdxSource={mdxSource}
+          frontMatter={frontMatter}
+          authorDetails={authorDetails}
+          prev={prev}
+          next={next}
+        />
+      ) : (
+        <div className="mt-24 text-center">
+          <PageTitle>
+            Under Construction{' '}
+            <span role="img" aria-label="roadwork sign">
+              🚧
+            </span>
+          </PageTitle>
+        </div>
+      )}
+    </>
+  )
 }
